f6c48e795bfc50fed12ae1f0ed13e0e52c5fd74a
Siriki-Devi <sirikidevi54@gmail.com>
2026-01-02 19:27:48 +0530

    Add files via upload

---
+++ SmartVoting/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from users.forms import VoterRegistrationForm
+
+
+def index(request):
+    return render(request, 'index.html', {})
+    # return render(request, 'GetUserPics.html', {})
+
+def AdminLogin(request):
+    return render(request, 'AdminLogin.html', {})
+
+def UserLogin(request):
+    return render(request, 'UserLogin.html', {})
+
+
+def UserRegister(request):
+    form = VoterRegistrationForm()
+    return render(request, 'UserRegistrations.html', {'form': form})
